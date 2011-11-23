7620ad76605fc173307dce998ebc7ec437e7f013
Shawn Lindberg <shawn.lindberg@gmail.com>
2011-11-23 18:24:23 +0000

    Speech input is now parsed and used as voice commands.

--- sandman/source/main.cpp
+++ sandman/source/main.cpp
@@ -13,6 +13,21 @@
 // Types
 //
 
+// Types of command tokens.
+enum CommandTokenTypes
+{
+	COMMAND_TOKEN_SAND,
+	COMMAND_TOKEN_MAN,
+	COMMAND_TOKEN_HEAD,
+	COMMAND_TOKEN_KNEE,
+	COMMAND_TOKEN_ELEVATION,
+	COMMAND_TOKEN_UP,
+	COMMAND_TOKEN_DOWN,
+	COMMAND_TOKEN_STOP,
+
+	NUM_COMMAND_TOKEN_TYPES,
+};
+
 // Types of controls.
 enum ControlTypes
 {
@@ -28,6 +43,19 @@ enum ControlTypes
 
 // Locals
 //
+
+// Names for each command token.
+static char const* const s_CommandTokenNames[] = 
+{
+	"sand",			// COMMAND_TOKEN_SAND
+	"man",			// COMMAND_TOKEN_MAN
+	"head",			// COMMAND_TOKEN_HEAD
+	"knee",			// COMMAND_TOKEN_KNEE
+	"elevation",	// COMMAND_TOKEN_ELEVATION
+	"up",			// COMMAND_TOKEN_UP
+	"down",			// COMMAND_TOKEN_DOWN
+	"stop",			// COMMAND_TOKEN_STOP
+};
 
 // The command string to send to the micro for each control.
 static char const* const s_ControlCommandStrings[] =
@@ -178,20 +206,20 @@ int main()
 
 	printf("\tsucceeded\n\n");
 
-	// Track utterances.
-	bool l_InUtterance = false;
-	int l_LastVoiceSampleCount = 0;
-
 	// Initialize controls.
 	for (unsigned int l_ControlIndex = 0; l_ControlIndex < NUM_CONTROL_TYPES; l_ControlIndex++)
 	{
 		s_Controls[l_ControlIndex].Initialize(&l_Serial, s_ControlCommandStrings[l_ControlIndex]);
 	}
 
-	// Store a command here.
-	unsigned int const l_CommandBufferSize = 8;
-	char l_CommandBuffer[l_CommandBufferSize];
-	unsigned int l_CommandLength = 0;
+	// Track utterances.
+	bool l_InUtterance = false;
+	int l_LastVoiceSampleCount = 0;
+
+	// Store a keyboard input here.
+	unsigned int const l_KeyboardInputBufferCapacity = 8;
+	char l_KeyboardInputBuffer[l_KeyboardInputBufferCapacity];
+	unsigned int l_KeyboardInputBufferSize = 0;
 
 	// Store serial string data here.
 	unsigned int const l_SerialStringBufferCapacity = 512;
@@ -208,69 +236,68 @@ int main()
 			char l_NextChar = static_cast<char>(_getch());
 
 			// Accumulate characters until we get a terminating character or we run out of space.
-			if ((l_NextChar != ' ') && (l_CommandLength < (l_CommandBufferSize - 1)))
+			if ((l_NextChar != ' ') && (l_KeyboardInputBufferSize < (l_KeyboardInputBufferCapacity - 1)))
 			{
-				l_CommandBuffer[l_CommandLength] = l_NextChar;
-				l_CommandLength++;
+				l_KeyboardInputBuffer[l_KeyboardInputBufferSize] = l_NextChar;
+				l_KeyboardInputBufferSize++;
 			}
 			else
 			{
 				// Terminate the command.
-				l_CommandBuffer[l_CommandLength] = '\0';
+				l_KeyboardInputBuffer[l_KeyboardInputBufferSize] = '\0';
 
 				// Echo the command back.
-				printf("Command input: \"%s\"\n", l_CommandBuffer);
+				printf("Keyboard command input: \"%s\"\n", l_KeyboardInputBuffer);
 
 				// Command handling.
-				if (l_CommandBuffer[0] == 'h')
+				if (l_KeyboardInputBuffer[0] == 'h')
 				{
-					if (l_CommandBuffer[1] == 'u')
+					if (l_KeyboardInputBuffer[1] == 'u')
 					{
 						s_Controls[CONTROL_HEAD_UP].SetMovingDesired(true);
 					}
-					else if (l_CommandBuffer[1] == 'd')
+					else if (l_KeyboardInputBuffer[1] == 'd')
 					{
 						s_Controls[CONTROL_HEAD_DOWN].SetMovingDesired(true);
 					}
 				}
-				else if (l_CommandBuffer[0] == 'k')
+				else if (l_KeyboardInputBuffer[0] == 'k')
 				{
-					if (l_CommandBuffer[1] == 'u')
+					if (l_KeyboardInputBuffer[1] == 'u')
 					{
 						s_Controls[CONTROL_KNEE_UP].SetMovingDesired(true);
 					}
-					else if (l_CommandBuffer[1] == 'd')
+					else if (l_KeyboardInputBuffer[1] == 'd')
 					{
 						s_Controls[CONTROL_KNEE_DOWN].SetMovingDesired(true);
 					}
 				}
-				else if (l_CommandBuffer[0] == 'e')
+				else if (l_KeyboardInputBuffer[0] == 'e')
 				{
-					if (l_CommandBuffer[1] == 'u')
+					if (l_KeyboardInputBuffer[1] == 'u')
 					{
 						s_Controls[CONTROL_ELEVATION_UP].SetMovingDesired(true);
 					}
-					else if (l_CommandBuffer[1] == 'd')
+					else if (l_KeyboardInputBuffer[1] == 'd')
 					{
 						s_Controls[CONTROL_ELEVATION_DOWN].SetMovingDesired(true);
 					}
 				}
-				else if (l_CommandBuffer[0] == 's')
+				else if (l_KeyboardInputBuffer[0] == 's')
 				{
 					// Stop controls.
 					for (unsigned int l_ControlIndex = 0; l_ControlIndex < NUM_CONTROL_TYPES; l_ControlIndex++)
 					{
 						s_Controls[l_ControlIndex].SetMovingDesired(false);
 					}
-
 				}
-				else if (l_CommandBuffer[0] == 'q')
+				else if (l_KeyboardInputBuffer[0] == 'q')
 				{
 					l_Done = true;
 				}
 
 				// Prepare for a new command.
-				l_CommandLength = 0;
+				l_KeyboardInputBufferSize = 0;
 			}
 		}
 
@@ -344,6 +371,177 @@ int main()
 					char const* l_RecognizedSpeech = ps_get_hyp(l_SpeechDecoder, &l_Score, &l_UtteranceID);
 
 					printf("Recognized: \"%s\", score %d, utterance ID \"%s\"\n", l_RecognizedSpeech, l_Score, l_UtteranceID);
+
+					// Parse a command.
+
+					// Store command tokens here.
+					unsigned int const l_CommandTokenBufferCapacity = 32;
+					CommandTokenTypes l_CommandTokenBuffer[l_CommandTokenBufferCapacity];
+					unsigned int l_CommandTokenBufferSize = 0;
+
+					// Store token strings here.
+					unsigned int const l_TokenStringBufferCapacity = 32;
+					char l_TokenStringBuffer[l_TokenStringBufferCapacity];
+
+					// Get the first token string start.
+					char const* l_NextTokenStringStart = l_RecognizedSpeech;
+
+					while (l_NextTokenStringStart != NULL)
+					{
+						// Get the next token string end.
+						char const* l_NextTokenStringEnd = strchr(l_NextTokenStringStart, ' ');
+
+						// Get the token string length.
+						unsigned int l_TokenStringLength = 0;
+						if (l_NextTokenStringEnd != NULL)
+						{
+							l_TokenStringLength = l_NextTokenStringEnd - l_NextTokenStringStart;
+						}
+						else 
+						{
+							l_TokenStringLength = strlen(l_NextTokenStringStart);
+						}
+
+						// Copy the token string.
+						unsigned int l_AmountToCopy = min(l_TokenStringBufferCapacity - 1, l_TokenStringLength);
+						strncpy_s(l_TokenStringBuffer, l_NextTokenStringStart, l_AmountToCopy);
+						l_TokenStringBuffer[l_AmountToCopy] = '\0';
+
+						// Make sure the token string is lowercase.
+						_strlwr_s(l_TokenStringBuffer);
+
+						// Match the token string to a token if possible.
+						for (unsigned int l_TokenType = 0; l_TokenType < NUM_COMMAND_TOKEN_TYPES; l_TokenType++)
+						{
+							// Compare the token string to its name.
+							if (strcmp(l_TokenStringBuffer, s_CommandTokenNames[l_TokenType]) != 0)
+							{
+								continue;
+							}
+
+							// Found a match.
+
+							// Add the token to the buffer.
+							if (l_CommandTokenBufferSize < l_CommandTokenBufferCapacity)
+							{
+								l_CommandTokenBuffer[l_CommandTokenBufferSize] = static_cast<CommandTokenTypes>(l_TokenType);
+								l_CommandTokenBufferSize++;
+							} 
+							else
+							{
+								printf("Voice command too long, tail will be ignored.\n");
+							}
+
+							break;
+						}
+
+						// Get the next token string start (skip delimiter).
+						if (l_NextTokenStringEnd != NULL)
+						{
+							l_NextTokenStringStart = l_NextTokenStringEnd + 1;
+						}
+						else
+						{
+							l_NextTokenStringStart = NULL;
+						}
+					}
+
+					// Parse command tokens.
+					for (unsigned int l_TokenIndex = 0; l_TokenIndex < l_CommandTokenBufferSize; l_TokenIndex++)
+					{
+						// Look for the prefix.
+						if (l_CommandTokenBuffer[l_TokenIndex] != COMMAND_TOKEN_SAND)
+						{
+							continue;
+						}
+
+						// Next token.
+						l_TokenIndex++;
+						if (l_TokenIndex >= l_CommandTokenBufferSize)
+						{
+							break;
+						}
+
+						// Look for the rest of the prefix.
+						if (l_CommandTokenBuffer[l_TokenIndex] != COMMAND_TOKEN_MAN)
+						{
+							continue;
+						}
+
+						// Next token.
+						l_TokenIndex++;
+						if (l_TokenIndex >= l_CommandTokenBufferSize)
+						{
+							break;
+						}
+
+						// Parse commands.
+						if (l_CommandTokenBuffer[l_TokenIndex] == COMMAND_TOKEN_HEAD)
+						{
+							// Next token.
+							l_TokenIndex++;
+							if (l_TokenIndex >= l_CommandTokenBufferSize)
+							{
+								break;
+							}
+
+							if (l_CommandTokenBuffer[l_TokenIndex] == COMMAND_TOKEN_UP)
+							{
+								s_Controls[CONTROL_HEAD_UP].SetMovingDesired(true);
+							}
+							else if (l_CommandTokenBuffer[l_TokenIndex] == COMMAND_TOKEN_DOWN)
+							{
+								s_Controls[CONTROL_HEAD_DOWN].SetMovingDesired(true);
+							}
+						}
+						else if (l_CommandTokenBuffer[l_TokenIndex] == COMMAND_TOKEN_KNEE)
+						{
+							// Next token.
+							l_TokenIndex++;
+							if (l_TokenIndex >= l_CommandTokenBufferSize)
+							{
+								break;
+							}
+
+							if (l_CommandTokenBuffer[l_TokenIndex] == COMMAND_TOKEN_UP)
+							{
+								s_Controls[CONTROL_KNEE_UP].SetMovingDesired(true);
+							}
+							else if (l_CommandTokenBuffer[l_TokenIndex] == COMMAND_TOKEN_DOWN)
+							{
+								s_Controls[CONTROL_KNEE_DOWN].SetMovingDesired(true);
+							}
+						}
+						else if (l_CommandTokenBuffer[l_TokenIndex] == COMMAND_TOKEN_ELEVATION)
+						{
+							// Next token.
+							l_TokenIndex++;
+							if (l_TokenIndex >= l_CommandTokenBufferSize)
+							{
+								break;
+							}
+
+							if (l_CommandTokenBuffer[l_TokenIndex] == COMMAND_TOKEN_UP)
+							{
+								s_Controls[CONTROL_ELEVATION_UP].SetMovingDesired(true);
+							}
+							else if (l_CommandTokenBuffer[l_TokenIndex] == COMMAND_TOKEN_DOWN)
+							{
+								s_Controls[CONTROL_ELEVATION_DOWN].SetMovingDesired(true);
+							}
+						}
+						else if (l_CommandTokenBuffer[l_TokenIndex] == COMMAND_TOKEN_STOP)
+						{
+							// Stop controls.
+							for (unsigned int l_ControlIndex = 0; l_ControlIndex < NUM_CONTROL_TYPES; l_ControlIndex++)
+							{
+								s_Controls[l_ControlIndex].SetMovingDesired(false);
+							}
+						}
+					}
+
+					// All tokens parsed.
+					l_CommandTokenBufferSize = 0;
 				}
 			}
 		}
