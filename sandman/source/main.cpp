f515fb6b42bc39f8a4d0a73df8948e87840abb1f
Shawn Lindberg <shawn.lindberg@gmail.com>
2020-03-29 22:10:11 -0500

    Proportional control is now fully implemented. Refactored the tokens so that we can now have integer token types, the first and probably only parameter tokens. Added some parsing code which can turn a token string into an integer. If a standard control command has a suffix which is an integer, it is considered to be the percentage of the full duration to move the control.

--- sandman/source/main.cpp
+++ sandman/source/main.cpp
@@ -31,25 +31,40 @@
 // Types
 //
 
-// Types of command tokens.
-enum CommandTokenTypes
+// Describes a command token.
+struct CommandToken
 {
-	COMMAND_TOKEN_INVALID = -1,
+	// Types of command tokens.
+	enum Types
+	{
+		TYPE_INVALID = -1,
 
-	COMMAND_TOKEN_BACK,
-	COMMAND_TOKEN_LEGS,
-	COMMAND_TOKEN_ELEVATION,
-	COMMAND_TOKEN_RAISE,
-	COMMAND_TOKEN_LOWER,
-	COMMAND_TOKEN_STOP,
-	COMMAND_TOKEN_VOLUME,
-	// COMMAND_TOKEN_MUTE,
-	// COMMAND_TOKEN_UNMUTE,
-	COMMAND_TOKEN_SCHEDULE,
-	COMMAND_TOKEN_START,
-	COMMAND_TOKEN_STATUS,
+		TYPE_BACK,
+		TYPE_LEGS,
+		TYPE_ELEVATION,
+		TYPE_RAISE,
+		TYPE_LOWER,
+		TYPE_STOP,
+		TYPE_VOLUME,
+		// TYPE_MUTE,
+		// TYPE_UNMUTE,
+		TYPE_SCHEDULE,
+		TYPE_START,
+		TYPE_STATUS,
+		
+		TYPE_NOT_PARAMETER_COUNT, 
+		
+		// The following command tokens are parameters.
+		TYPE_INTEGER = TYPE_NOT_PARAMETER_COUNT, 
+		
+		TYPE_COUNT,
+	};
+
+	// The type of the token.
+	Types	m_Type = TYPE_INVALID;
 	
-	NUM_COMMAND_TOKEN_TYPES,
+	// The value of the integer parameter, if relevant.
+	int m_Parameter = 0;
 };
 
 // Locals
@@ -58,18 +73,20 @@ enum CommandTokenTypes
 // Names for each command token.
 static char const* const s_CommandTokenNames[] = 
 {
-	"back",			// COMMAND_TOKEN_BACK
-	"legs",			// COMMAND_TOKEN_LEGS
-	"elevation",	// COMMAND_TOKEN_ELEVATION
-	"raise",			// COMMAND_TOKEN_RAISE
-	"lower",			// COMMAND_TOKEN_LOWER
-	"stop",			// COMMAND_TOKEN_STOP
-	"volume",		// COMMAND_TOKEN_VOLUME
-	// "mute",			// COMMAND_TOKEN_MUTE
-	// "unmute",		// COMMAND_TOKEN_UNMUTE
-	"schedule",		// COMMAND_TOKEN_SCHEDULE
-	"start",			// COMMAND_TOKEN_START
-	"status",		// COMMAND_TOKEN_STATUS
+	"back",			// TYPE_BACK
+	"legs",			// TYPE_LEGS
+	"elevation",	// TYPE_ELEVATION
+	"raise",			// TYPE_RAISE
+	"lower",			// TYPE_LOWER
+	"stop",			// TYPE_STOP
+	"volume",		// TYPE_VOLUME
+	// "mute",			// TYPE_MUTE
+	// "unmute",		// TYPE_UNMUTE
+	"schedule",		// TYPE_SCHEDULE
+	"start",			// TYPE_START
+	"status",		// TYPE_STATUS
+	
+	"integer", 		// TYPE_INTEGER
 };
 
 // Whether controls have been initialized.
@@ -120,6 +137,79 @@ static void ConvertStringToLowercase(char* p_String)
 		// Next letter.
 		l_CurrentLetter++;
 	}
+}
+
+// Converts a string to an (unsigned) integer.
+//
+// p_Integer:	(Output) The converted integer value.
+// p_String:	The string to convert.
+// 
+// Returns:	True if a valid integer was created, false otherwise.
+//
+static bool ConvertStringToInteger(int& p_Integer, char* p_String)
+{
+	// Sanity check.
+	if (p_String == nullptr)
+	{
+		return false;
+	}
+	
+	if (p_String[0] == '\0')
+	{
+		return false;
+	}
+	
+	// Build up the integer in a larger container, so overflow can be easily detected.
+	int64_t l_Integer = 0;
+	
+	auto* l_CurrentCharacter = p_String;
+	
+	while (*l_CurrentCharacter != '\0')
+	{
+		// Convert this letter.
+		switch (*l_CurrentCharacter)
+		{
+			case '0':	// Fall through...
+			case '1':	// Fall through...
+			case '2':	// Fall through...
+			case '3':	// Fall through...
+			case '4':	// Fall through...
+			case '5':	// Fall through...
+			case '6':	// Fall through...
+			case '7':	// Fall through...
+			case '8':	// Fall through...
+			case '9':
+			{
+				// Consider leading zeros to be badly formed.
+				if ((l_Integer == 0) && (l_CurrentCharacter != p_String)) {
+					return false;
+				}
+			
+				// This calculation depends on the ASCII character values starting at zero and 
+				// going up to nine contiguously.
+				auto const l_Digit = (*l_CurrentCharacter - '0');
+				l_Integer = (l_Integer * 10) + l_Digit;
+			}
+			break;
+			
+			default:
+			{
+			}
+			return false;
+		}
+
+		// If the number has gotten too large, bail.
+		if (l_Integer > INT_MAX) {
+			return false;
+		}
+		
+		// Next letter.
+		l_CurrentCharacter++;
+	}
+	
+	// Convert the accumulated value.
+	p_Integer = static_cast<int>(l_Integer);
+	return true;
 }
 
 // Initialize program components.
@@ -367,7 +457,7 @@ static void Uninitialize()
 // p_CommandTokens:	(Output) The resulting command tokens, in order.
 // p_CommandString:	The command string to tokenize.
 //
-static void TokenizeCommandString(std::vector<CommandTokenTypes>& p_CommandTokens, 
+static void TokenizeCommandString(std::vector<CommandToken>& p_CommandTokens, 
 	char const* p_CommandString)
 {
 	// Store token strings here.
@@ -400,10 +490,11 @@ static void TokenizeCommandString(std::vector<CommandTokenTypes>& p_CommandToken
 		// Make sure the token string is lowercase.
 		ConvertStringToLowercase(l_TokenStringBuffer);
 
-		// Match the token string to a token if possible.
-		auto l_Token = COMMAND_TOKEN_INVALID;
+		// Match the token string to a token (with no parameter) if possible.
+		CommandToken l_Token;
 
-		for (unsigned int l_TokenType = 0; l_TokenType < NUM_COMMAND_TOKEN_TYPES; l_TokenType++)
+		for (unsigned int l_TokenType = 0; l_TokenType < CommandToken::TYPE_NOT_PARAMETER_COUNT; 
+			l_TokenType++)
 		{
 			// Compare the token string to its name.
 			if (strcmp(l_TokenStringBuffer, s_CommandTokenNames[l_TokenType]) != 0)
@@ -412,10 +503,19 @@ static void TokenizeCommandString(std::vector<CommandTokenTypes>& p_CommandToken
 			}
 
 			// Found a match.
-			l_Token = static_cast<CommandTokenTypes>(l_TokenType);
+			l_Token.m_Type = static_cast<CommandToken::Types>(l_TokenType);
 			break;
 		}
 
+		// If we couldn't turn it into a plain old token, see if it is a parameter token.
+		if (l_Token.m_Type == CommandToken::TYPE_INVALID)
+		{
+			if (ConvertStringToInteger(l_Token.m_Parameter, l_TokenStringBuffer) == true)
+			{
+				l_Token.m_Type = CommandToken::TYPE_INTEGER;
+			}
+		}
+		
 		// Add the token to the list.
 		p_CommandTokens.push_back(l_Token);
 
@@ -435,7 +535,7 @@ static void TokenizeCommandString(std::vector<CommandTokenTypes>& p_CommandToken
 //
 // p_CommandTokens:	All of the potential tokens for the command.
 //
-static void ParseCommandTokens(std::vector<CommandTokenTypes> const& p_CommandTokens)
+static void ParseCommandTokens(std::vector<CommandToken> const& p_CommandTokens)
 {
 	// Parse command tokens.
 	auto const l_TokenCount = static_cast<unsigned int>(p_CommandTokens.size());
@@ -443,18 +543,18 @@ static void ParseCommandTokens(std::vector<CommandTokenTypes> const& p_CommandTo
 	{
 		// Parse commands.
 		auto l_Token = p_CommandTokens[l_TokenIndex];
-		switch (l_Token)
+		switch (l_Token.m_Type)
 		{
-			case COMMAND_TOKEN_BACK:		// Fall through...
-			case COMMAND_TOKEN_LEGS:		// Fall through...
-			case COMMAND_TOKEN_ELEVATION:	
+			case CommandToken::TYPE_BACK:			// Fall through...
+			case CommandToken::TYPE_LEGS:			// Fall through...
+			case CommandToken::TYPE_ELEVATION:	
 			{
 				// Try to access the control corresponding to the command token.
 				Control* l_Control = nullptr;
 				
-				switch (l_Token)
+				switch (l_Token.m_Type)
 				{
-					case COMMAND_TOKEN_BACK:
+					case CommandToken::TYPE_BACK:
 					{
 						// Try to find the control.
 						if (s_BackControlHandle.IsValid() == false)
@@ -466,7 +566,7 @@ static void ParseCommandTokens(std::vector<CommandTokenTypes> const& p_CommandTo
 					}
 					break;
 					
-					case COMMAND_TOKEN_LEGS:
+					case CommandToken::TYPE_LEGS:
 					{
 						// Try to find the control.
 						if (s_LegsControlHandle.IsValid() == false)
@@ -478,7 +578,7 @@ static void ParseCommandTokens(std::vector<CommandTokenTypes> const& p_CommandTo
 					}
 					break;
 					
-					case COMMAND_TOKEN_ELEVATION:
+					case CommandToken::TYPE_ELEVATION:
 					{
 						// Try to find the control.
 						if (s_ElevationControlHandle.IsValid() == false)
@@ -493,7 +593,7 @@ static void ParseCommandTokens(std::vector<CommandTokenTypes> const& p_CommandTo
 					default:
 					{
 						LoggerAddMessage("Unrecognized token \"%s\" trying to process a control movement "
-							"command.", s_CommandTokenNames[l_Token]);
+							"command.", s_CommandTokenNames[l_Token.m_Type]);
 					}
 					break;
 				}
@@ -512,25 +612,54 @@ static void ParseCommandTokens(std::vector<CommandTokenTypes> const& p_CommandTo
 				
 				l_Token = p_CommandTokens[l_TokenIndex];
 				
-				if (l_Token == COMMAND_TOKEN_RAISE)
+				// Try to get the action which should be performed on the control.
+				auto l_Action = Control::ACTION_STOPPED;
+				
+				if (l_Token.m_Type == CommandToken::TYPE_RAISE)
 				{
-					l_Control->SetDesiredAction(Control::ACTION_MOVING_UP, Control::MODE_TIMED);
+					l_Action = Control::ACTION_MOVING_UP;
 				}
-				else if (l_Token == COMMAND_TOKEN_LOWER)
+				else if (l_Token.m_Type == CommandToken::TYPE_LOWER)
 				{
-					l_Control->SetDesiredAction(Control::ACTION_MOVING_DOWN, Control::MODE_TIMED);
+					l_Action = Control::ACTION_MOVING_DOWN;
 				}
+				
+				if (l_Action == Control::ACTION_STOPPED)
+				{
+					break;
+				}
+				
+				// Determine the duration percent.
+				unsigned int l_DurationPercent = 100;
+				
+				// Peak at the next token.
+				auto const l_NextTokenIndex = l_TokenIndex + 1;
+				if (l_NextTokenIndex < l_TokenCount)
+				{
+					auto const l_NextToken = p_CommandTokens[l_NextTokenIndex];
+					
+					if (l_NextToken.m_Type == CommandToken::TYPE_INTEGER)
+					{
+						l_DurationPercent = l_NextToken.m_Parameter;
+						
+						// Actually consume this token.
+						l_TokenIndex++;
+					}
+				}
+				
+				
+				l_Control->SetDesiredAction(l_Action, Control::MODE_TIMED, l_DurationPercent);
 			}
 			break;
 			
-			case COMMAND_TOKEN_STOP:
+			case CommandToken::TYPE_STOP:
 			{
 				// Stop controls.
 				ControlsStopAll();			
 			}
 			break;
 			
-			case COMMAND_TOKEN_SCHEDULE:
+			case CommandToken::TYPE_SCHEDULE:
 			{
 				// Next token.
 				l_TokenIndex++;
@@ -541,18 +670,18 @@ static void ParseCommandTokens(std::vector<CommandTokenTypes> const& p_CommandTo
 				
 				l_Token = p_CommandTokens[l_TokenIndex];
 			
-				if (l_Token == COMMAND_TOKEN_START)
+				if (l_Token.m_Type == CommandToken::TYPE_START)
 				{
 					ScheduleStart();
 				}
-				else if (l_Token == COMMAND_TOKEN_STOP)
+				else if (l_Token.m_Type == CommandToken::TYPE_STOP)
 				{
 					ScheduleStop();
 				}			
 			}
 			break;
 			
-			case COMMAND_TOKEN_VOLUME:
+			case CommandToken::TYPE_VOLUME:
 			{
 				// Next token.
 				l_TokenIndex++;
@@ -563,30 +692,30 @@ static void ParseCommandTokens(std::vector<CommandTokenTypes> const& p_CommandTo
 
 				l_Token = p_CommandTokens[l_TokenIndex];
 				
-				if (l_Token == COMMAND_TOKEN_RAISE)
+				if (l_Token.m_Type == CommandToken::TYPE_RAISE)
 				{
 					SoundIncreaseVolume();
 				}
-				else if (l_Token == COMMAND_TOKEN_LOWER)
+				else if (l_Token.m_Type == CommandToken::TYPE_LOWER)
 				{
 					SoundDecreaseVolume();
 				}
 			}
 			break;
 			
-			// case COMMAND_TOKEN_MUTE:
+			// case CommandToken::TYPE_MUTE:
 			// {
 			// 	SoundMute(true);
 			// }
 			// break;
 			
-			// case COMMAND_TOKEN_UNMUTE:
+			// case CommandToken::TYPE_UNMUTE:
 			// {
 			// 	SoundMute(false);
 			// }
 			// break;
 			
-			case COMMAND_TOKEN_STATUS:
+			case CommandToken::TYPE_STATUS:
 			{
 				// Play status speech.
 				SoundAddToQueue(DATADIR "audio/running.wav");	
@@ -649,7 +778,7 @@ static bool ProcessKeyboardInput(char* p_KeyboardInputBuffer, unsigned int& p_Ke
 	// Parse a command.
 
 	// Tokenize the string.
-	std::vector<CommandTokenTypes> l_CommandTokens;
+	std::vector<CommandToken> l_CommandTokens;
 	TokenizeCommandString(l_CommandTokens, p_KeyboardInputBuffer);
 
 	// Parse command tokens.
@@ -716,7 +845,7 @@ static bool ProcessSocketCommunication()
 		// Parse a command.
 
 		// Tokenize the message.
-		std::vector<CommandTokenTypes> l_CommandTokens;
+		std::vector<CommandToken> l_CommandTokens;
 		TokenizeCommandString(l_CommandTokens,	l_MessageBuffer);
 
 		// Parse command tokens.
