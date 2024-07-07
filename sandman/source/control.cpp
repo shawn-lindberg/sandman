efaa7f19859433b25128471a79cd62716d0096b5
Adrian <73155005+serene-meadow@users.noreply.github.com>
2024-07-07 04:08:40 -0500

    fix format bug

--- sandman/source/control.cpp
+++ sandman/source/control.cpp
@@ -384,7 +384,7 @@ void Control::Process()
 
 		default:
 		{
-			LoggerAddMessage("Control \"%s\": Unrecognized state %d in Process()", m_State, m_Name);
+			LoggerAddMessage("Control \"%i\": Unrecognized state %s in Process()", m_State, m_Name);
 		}
 		break;
 	}
