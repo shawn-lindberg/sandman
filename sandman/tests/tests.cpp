f4b6ac92336ab2c018fb5c233b05f418603cdf52
Shawn Lindberg <shawn.lindberg@gmail.com>
2024-07-07 22:07:25 -0500

    And more tests for the invalid and example schedule. Add a Catch2 event listener to set up and shut down the logging system when the test run begins and ends.

--- sandman/tests/tests.cpp
+++ sandman/tests/tests.cpp
@@ -1,6 +1,25 @@
 #include "catch_amalgamated.hpp"
 
+#include "logger.h"
 #include "schedule.h"
+
+class TestRunListener : public Catch::EventListenerBase
+{
+public:
+	using Catch::EventListenerBase::EventListenerBase;
+
+   void testRunStarting(Catch::TestRunInfo const&) override
+	{
+		LoggerInitialize("tests.log");
+	}
+
+	void testRunEnded(Catch::TestRunStats const&) override
+	{
+		LoggerUninitialize();
+	}
+};
+
+CATCH_REGISTER_LISTENER(TestRunListener)
 
 TEST_CASE("Test missing schedule", "[schedules]")
 {
@@ -9,10 +28,25 @@ TEST_CASE("Test missing schedule", "[schedules]")
 	REQUIRE(l_Loaded == false);
 }
 
-TEST_CASE("Test default schedule", "[schedules]")
+TEST_CASE("Test default (empty) schedule", "[schedules]")
 {
 	Schedule l_Schedule;
 	const bool l_Loaded = l_Schedule.LoadFromFile("data/sandman.sched");
 	REQUIRE(l_Loaded == true);
 	REQUIRE(l_Schedule.IsEmpty() == true);
+}
+
+TEST_CASE("Test invalid schedule", "[schedules]")
+{
+	Schedule l_Schedule;
+	const bool l_Loaded = l_Schedule.LoadFromFile("data/invalidJson.sched");
+	REQUIRE(l_Loaded == false);
+}
+
+TEST_CASE("Test example schedule", "[schedules]")
+{
+	Schedule l_Schedule;
+	const bool l_Loaded = l_Schedule.LoadFromFile("data/example.sched");
+	REQUIRE(l_Loaded == true);
+	REQUIRE(l_Schedule.IsEmpty() == false);
 }
