296ad26eb503ed59b53097466c247d4382b7f34d
Adrian <73155005+serene-meadow@users.noreply.github.com>
2024-07-10 21:08:38 -0500

    annotate functions with Doxygen comments

--- sandman/source/logger.h
+++ sandman/source/logger.h
@@ -1,3 +1,4 @@
+/// @file
 #pragma once
 
 #include <stdarg.h>
@@ -8,39 +9,39 @@
 // Functions
 //
 
-// Initialize the logger.
-//
-// p_LogFileName:	File name of the log for output.
-//
-// returns:		True if successful, false otherwise.
-//
+/// @brief Initialize the logger.
+///
+/// @param p_LogFileName File name of the log for output.
+///
+/// @returns `true` if successful, `false` otherwise.
+///
 bool LoggerInitialize(char const* p_LogFileName);
 
-// Uninitialize the logger.
-//
+/// @brief Uninitialize the logger.
+///
 void LoggerUninitialize();
 
-// Set whether to echo messages to the screen as well.
-//
-// p_LogToScreen:	Whether to echo messages to the screen.
-//
+/// @brief Set whether to echo messages to the screen as well.
+///
+/// @param p_LogToScreen Whether to echo messages to the screen.
+///
 void LoggerEchoToScreen(bool p_LogToScreen);
 
-// Add a message to the log.
-//
-// p_Format:	Standard printf format string.
-// ...:			Standard printf arguments.
-//
-// returns:		True if successful, false otherwise.
-//
+/// @brief Add a message to the log.
+///
+/// @param p_Format Standard printf format string.
+/// @param ... Standard printf arguments.
+///
+/// @returns `true` if successful, `false` otherwise.
+///
 bool LoggerAddMessage(char const* p_Format, ...);
 
-// Add a message to the log (va_list version).
-//
-// p_Format:		Standard printf format string.
-// p_Arguments:	Standard printf arguments.
-//
-// returns:		True if successful, false otherwise.
-//
+/// @brief Add a message to the log (`va_list` version).
+///
+/// @param p_Format Standard printf format string.
+/// @param p_Arguments Standard printf arguments.
+///
+/// @returns `true` if successful, `false` otherwise.
+///
 bool LoggerAddMessage(char const* p_Format, va_list& p_Arguments);
 
