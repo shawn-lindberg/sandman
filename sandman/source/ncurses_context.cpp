5112bb50e63cd06af19af509a9d3ea28c1d8489f
Adrian <73155005+serene-meadow@users.noreply.github.com>
2024-07-11 23:03:30 -0500

    Put `NCurses` function definitions in the namespace itself instead of just qualifying with `NCurses::`.

--- sandman/source/ncurses_context.cpp
+++ sandman/source/ncurses_context.cpp
@@ -59,86 +59,86 @@ namespace NCurses
 		}
 	}
 
+	void Initialize()
+	{
+		/*
+			Initialize NCurses. This function exits the program on error!
+			This initializes the standard screen `stdscr` window.
+		*/
+		initscr();
+
+		// input options: `man 'inopts(3NCURSES)'`
+		// These options in particular do not take a window pointer.
+		{
+			/*
+				Don't have to wait for newlines in order to get characters.
+				Control characters are treated normally.
+				Control characters on Linux include Ctrl+Z (suspend), Ctrl+C (quit), etc.
+
+				Note: it is not recomended to call `raw` or `noraw` after setting `cbreak` or `nocbreak`
+				according to "NOTES" section of `man 'inopts(3NCURSES)'`.
+			*/
+			cbreak();
+
+			/*
+				Do not echo the characters typed by the user into the terminal by default.
+				Echo of the user input should be implemented manually for this project.
+			*/
+			noecho();
+
+			// No newlines; translate newline characters to carriage return characters.
+			nonl();
+		}
+
+		// Configure standard screen window.
+		ConfigureWindowDefaults(stdscr);
+
+		// The input window will have a height of 3.
+		static constexpr int s_InputWindowRowCount{ 3 };
+
+		// configure logging window
+		{
+			s_LoggingWindow = newwin(/* height (line count) */ LINES - s_InputWindowRowCount,
+											 /* width               */ COLS,
+											 /* upper corner y      */ 0,
+											 /* left-hand corner x  */ 0);
+
+			ConfigureWindowDefaults(s_LoggingWindow);
+
+			// Scroll when cursor is moved off the edge of the window.
+			scrollok(s_LoggingWindow, TRUE);
+		}
+
+		// configure input window
+		{
+			s_InputWindow = newwin(/* height (line count) */ s_InputWindowRowCount,
+										  /* width               */ COLS,
+										  /* upper corner y      */ LINES - s_InputWindowRowCount,
+										  /* left-hand corner x  */ 0);
+
+			ConfigureWindowDefaults(s_InputWindow);
+
+			// Draw a border on the window.
+			box(s_InputWindow, 0 /* use default vertical character */,
+				 0 /* use default horizontal character */);
+
+			// Move the cursor to the corner
+			wmove(s_InputWindow, INPUT_WINDOW_CURSOR_START_Y, INPUT_WINDOW_CURSOR_START_X);
+		}
+
+		// Clear the screen.
+		clear();
+	}
+
+	void Uninitialize()
+	{
+		delwin(s_LoggingWindow);
+		s_LoggingWindow = nullptr;
+
+		delwin(s_InputWindow);
+		s_InputWindow = nullptr;
+
+		endwin();
+	}
+
 } // namespace NCurses
-
-void NCurses::Initialize()
-{
-	/*
-		Initialize NCurses. This function exits the program on error!
-		This initializes the standard screen `stdscr` window.
-	*/
-	initscr();
-
-	// input options: `man 'inopts(3NCURSES)'`
-	// These options in particular do not take a window pointer.
-	{
-		/*
-			Don't have to wait for newlines in order to get characters.
-			Control characters are treated normally.
-			Control characters on Linux include Ctrl+Z (suspend), Ctrl+C (quit), etc.
-
-			Note: it is not recomended to call `raw` or `noraw` after setting `cbreak` or `nocbreak`
-			according to "NOTES" section of `man 'inopts(3NCURSES)'`.
-		*/
-		cbreak();
-
-		/*
-			Do not echo the characters typed by the user into the terminal by default.
-			Echo of the user input should be implemented manually for this project.
-		*/
-		noecho();
-
-		// No newlines; translate newline characters to carriage return characters.
-		nonl();
-	}
-
-	// Configure standard screen window.
-	ConfigureWindowDefaults(stdscr);
-
-	// The input window will have a height of 3.
-	static constexpr int s_InputWindowRowCount{ 3 };
-
-	// configure logging window
-	{
-		s_LoggingWindow = newwin(/* height (line count) */ LINES - s_InputWindowRowCount,
-										 /* width               */ COLS,
-										 /* upper corner y      */ 0,
-										 /* left-hand corner x  */ 0);
-
-		ConfigureWindowDefaults(s_LoggingWindow);
-
-		// Scroll when cursor is moved off the edge of the window.
-		scrollok(s_LoggingWindow, TRUE);
-	}
-
-	// configure input window
-	{
-		s_InputWindow = newwin(/* height (line count) */ s_InputWindowRowCount,
-									  /* width               */ COLS,
-									  /* upper corner y      */ LINES - s_InputWindowRowCount,
-									  /* left-hand corner x  */ 0);
-
-		ConfigureWindowDefaults(s_InputWindow);
-
-		// Draw a border on the window.
-		box(s_InputWindow, 0 /* use default vertical character */,
-			 0 /* use default horizontal character */);
-
-		// Move the cursor to the corner
-		wmove(s_InputWindow, INPUT_WINDOW_CURSOR_START_Y, INPUT_WINDOW_CURSOR_START_X);
-	}
-
-	// Clear the screen.
-	clear();
-}
-
-void NCurses::Uninitialize()
-{
-	delwin(s_LoggingWindow);
-	s_LoggingWindow = nullptr;
-
-	delwin(s_InputWindow);
-	s_InputWindow = nullptr;
-
-	endwin();
-}
