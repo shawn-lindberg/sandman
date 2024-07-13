1cebd457c8aff484e1e1a06019b420c65efe70fc
Adrian <73155005+serene-meadow@users.noreply.github.com>
2024-07-12 19:52:53 -0500

    update comment

--- sandman/source/ncurses_ui.h
+++ sandman/source/ncurses_ui.h
@@ -83,7 +83,7 @@ namespace NCurses
 	/// @returns `true` if `p_Character` is a value that fits into the ASCII character set, `false`
 	/// otherwise.
 	///
-	/// @note This is an alternative to the POSIX function `isascii`, because `isascii` is deprecated
+	/// @note This is an alternative to the POSIX function `isascii`. `isascii` is deprecated
 	/// and depends on the locale. See `STANDARDS` section of `man 'isalpha(3)'`: "POSIX.1-2008 marks
 	/// `isascii` as obsolete, noting that it cannot be used portably in a localized application."
 	/// Furthermore, `isascii` is part of the POSIX standard, but is not part of the C or C++
