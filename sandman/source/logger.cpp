3f38bb6176d8dca4e4fcab4e2a9557bc0c31959a
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-07-20 02:54:51 -0500

    trim unneeded comments

--- sandman/source/logger.cpp
+++ sandman/source/logger.cpp
@@ -1,66 +1,11 @@
 #include "logger.h"
 
-#if defined (__linux__)
-	#include "ncurses_ui.h"
-#endif // defined (__linux__)
-
-#include <cstdio>
-#include <cstring>
-#include <ctime>
-#include <mutex>
-#include <fstream>
-
 
 bool Logger::g_ScreenEcho{ false };
 
 std::mutex Logger::Self::s_Mutex;
 std::ofstream Logger::Self::s_File;
 std::ostringstream Logger::Self::s_Buffer;
-
-// Locals
-//
-
-// Used to enforce serialization of messages.
-// std::mutex s_LogMutex;
-
-// The file to log messages to.
-// static std::FILE* s_LogFile = nullptr;
-
-// Whether to echo messages to the screen.
-// static bool s_LogToScreen = false;
-
-// Functions
-//
-
-// Initialize the logger.
-//
-// p_LogFileName:	File name of the log for output.
-//
-// returns:		True if successful, false otherwise.
-//
-// bool LoggerInitialize(char const* p_LogFileName)
-// {
-// 	// Acquire a lock for the rest of the function.
-// 	std::lock_guard<std::mutex> const l_LogGuard(s_LogMutex);
-
-// 	// Initialize the file.
-// 	s_LogFile = nullptr;
-
-// 	if (p_LogFileName == nullptr)
-// 	{
-// 		return false;
-// 	}
-
-// 	// Try to open (and destroy old log).
-// 	s_LogFile = std::fopen(p_LogFileName, "w");
-
-// 	if (s_LogFile == nullptr)
-// 	{
-// 		return false;
-// 	}
-
-// 	return true;
-// }
 
 bool ::Logger::Initialize(char const* const p_LogFileName)
 {
@@ -69,22 +14,6 @@ bool ::Logger::Initialize(char const* const p_LogFileName)
 	Self::s_File = std::ofstream(p_LogFileName);
 	return true;
 }
-
-// Uninitialize the logger.
-//
-// void LoggerUninitialize()
-// {
-// 	// Acquire a lock for the rest of the function.
-// 	std::lock_guard<std::mutex> const l_LogGuard(s_LogMutex);
-
-// 	// Close the file.
-// 	if (s_LogFile != nullptr)
-// 	{
-// 		std::fclose(s_LogFile);
-// 	}
-
-// 	s_LogFile = nullptr;
-// }
 
 void Logger::Uninitialize()
 {
@@ -106,116 +35,3 @@ void Logger::Uninitialize()
 
 	return true;
 }
-
-// Set whether to echo messages to the screen as well.
-//
-// p_LogToScreen:	Whether to echo messages to the screen.
-//
-// void LoggerEchoToScreen(bool p_LogToScreen)
-// {
-// 	s_LogToScreen = p_LogToScreen;
-// }
-
-// Add a message to the log.
-//
-// p_Format:	Standard printf format string.
-// ...:			Standard printf arguments.
-//
-// returns:		True if successful, false otherwise.
-//
-// bool Logger::FormatWriteLine(char const* p_Format, ...)
-// {
-// 	std::va_list l_Arguments;
-// 	va_start(l_Arguments, p_Format);
-
-// 	auto const l_Result = Logger::FormatWriteLine(p_Format, l_Arguments);
-
-// 	va_end(l_Arguments);
-
-// 	return l_Result;
-// }
-
-// Add a message to the log (va_list version).
-//
-// p_Format:		Standard printf format string.
-// p_Arguments:	Standard printf arguments.
-//
-// returns:		True if successful, false otherwise.
-//
-// bool Logger::FormatWriteLine(char const* p_Format, va_list& p_Arguments)
-// {
-// 	static constexpr unsigned int s_LogStringBufferCapacity{ 2048u };
-// 	char l_LogStringBuffer[s_LogStringBufferCapacity];
-
-// 	// Initialize buffer write parameters.
-// 	auto l_RemainingCapacity = s_LogStringBufferCapacity;
-// 	auto* l_RemainingBuffer = l_LogStringBuffer;
-
-// 	// Get the time.
-// 	auto const l_RawTime = std::time(nullptr);
-// 	auto* l_LocalTime = std::localtime(&l_RawTime);
-
-// 	// Put the date and time in the buffer in 2012/09/23 17:44:05 CDT format.
-// 	std::strftime(l_RemainingBuffer, l_RemainingCapacity, "%Y/%m/%d %H:%M:%S %Z", l_LocalTime);
-
-// 	// Force terminate.
-// 	l_RemainingBuffer[l_RemainingCapacity - 1] = '\0';
-
-// 	// Update buffer write parameters.
-// 	unsigned int const l_CapacityUsed = std::strlen(l_RemainingBuffer);
-// 	l_RemainingBuffer += l_CapacityUsed;
-// 	l_RemainingCapacity -= l_CapacityUsed;
-
-// 	// Add a separator.
-// 	if (l_RemainingCapacity < 2u)
-// 	{
-// 		return false;
-// 	}
-
-// 	l_RemainingBuffer[0] = ']';
-// 	l_RemainingBuffer[1] = ' ';
-// 	l_RemainingBuffer += 2;
-// 	l_RemainingCapacity -= 2;
-
-// 	// Add the log message.
-// 	if (std::vsnprintf(l_RemainingBuffer, l_RemainingCapacity, p_Format, p_Arguments) < 0)
-// 	{
-// 		return false;
-// 	}
-
-// 	// Force terminate.
-// 	l_RemainingBuffer[l_RemainingCapacity - 1] = '\0';
-
-// 	{
-// 		// Acquire a lock for the rest of the function.
-// 		std::lock_guard<std::mutex> const l_LogGuard(s_LogMutex);
-
-// 		// Print to standard output (and add a newline).
-// 		if (s_LogToScreen == true)
-// 		{
-// 			#if defined (_WIN32)
-
-// 			std::puts(l_LogStringBuffer);
-
-// 			#elif defined (__linux__)
-
-// 			NCurses::Lock l;
-// 			NCurses::LoggingWindow::WriteLine(l_LogStringBuffer);
-
-// 			#endif // defined (_WIN32)
-// 		}
-
-// 		// Print to log file.
-// 		if (s_LogFile != nullptr)
-// 		{
-// 			std::fputs(l_LogStringBuffer, s_LogFile);
-
-// 			// fputs doesn't add a newline, do it now.
-// 			std::fputs("\n", s_LogFile);
-
-// 			std::fflush(s_LogFile);
-// 		}
-// 	}
-
-// 	return true;
-// }
--- sandman/source/logger.h
+++ sandman/source/logger.h
@@ -3,88 +3,12 @@
 #include <cstdarg>
 #include <sstream>
 #include <chrono>
-#include <array>
 #include <iomanip>
 #include <mutex>
 #include <fstream>
 #include <string>
 
 #include "ncurses_ui.h"
-
-// Types
-//
-
-// Functions
-//
-
-// Initialize the logger.
-//
-// p_LogFileName:	File name of the log for output.
-//
-// returns:		True if successful, false otherwise.
-//
-// bool LoggerInitialize(char const* p_LogFileName);
-
-// Uninitialize the logger.
-//
-// void LoggerUninitialize();
-
-// Set whether to echo messages to the screen as well.
-//
-// p_LogToScreen:	Whether to echo messages to the screen.
-//
-// void LoggerEchoToScreen(bool p_LogToScreen);
-
-// Add a message to the log.
-//
-// p_Format:	Standard printf format string.
-// ...:			Standard printf arguments.
-//
-// returns:		True if successful, false otherwise.
-//
-// [[gnu::format(printf, 1, 2)]] bool Logger::FormatWriteLine(char const* p_Format, ...);
-
-// Add a message to the log (va_list version).
-//
-// p_Format:		Standard printf format string.
-// p_Arguments:	Standard printf arguments.
-//
-// returns:		True if successful, false otherwise.
-//
-// [[gnu::format(printf, 1, 0)]] bool Logger::FormatWriteLine(char const* p_Format,
-// 																	 std::va_list& p_Arguments);
-
-/// @brief Log an empty line. (The logger still prints the date and time.)
-/// 
-/// @note `Logger::FormatWriteLine("")` triggers a warning `zero-length gnu_printf format string
-/// [-Werror=format-zero-length]`; this function accomplishes the same without triggering a warning.
-/// 
-/// @return `true` if successful, `false` otherwise
-///
-// [[gnu::always_inline]] inline bool Logger::WriteLine()
-// {
-// 	return Logger::FormatWriteLine("%s", "");
-// }
-
-/// @brief Log a variable amount of arguments.
-/// 
-/// @note This function uses an output string stream `std::ostringstream`
-/// to convert its arguments to a string which is passed to `Logger::FormatWriteLine`.
-/// So, this function can be used as a quick print statement when debugging,
-/// or as shorthand for when you would like to make use of `std::ostringstream`
-/// to format a string and log it.
-/// 
-/// @param p_Arguments arguments that are printed using the logger
-/// 
-/// @return `true` if successful, `false` otherwise
-/// 
-/// @attention This function constructs and destroys a `std::ostringstream` every call.
-///
-// template <typename... ParamsT>
-// [[gnu::always_inline]] inline bool LoggerPrintArgs(ParamsT const&... p_Arguments)
-// {
-// 	return Logger::FormatWriteLine("%s", (std::ostringstream() << ... << p_Arguments).str().c_str());
-// }
 
 namespace Logger
 {
