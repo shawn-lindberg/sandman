ae23c8cd13eb761a312550c3e2cc69880f75002e
Shawn Lindberg <shawn.lindberg@gmail.com>
2024-07-28 21:07:54 -0500

    Finish fleshing out the default config tests. Fixed some coding standards discrepancies.

--- sandman/tests/tests.cpp
+++ sandman/tests/tests.cpp
@@ -26,17 +26,17 @@ CATCH_REGISTER_LISTENER(TestRunListener)
 TEST_CASE("Test missing config", "[config]")
 {
 	Config l_Config;
-	const bool l_Loaded = l_Config.ReadFromFile("");
+	bool const l_Loaded = l_Config.ReadFromFile("");
 	REQUIRE(l_Loaded == false);
 }
 
 TEST_CASE("Test default config", "[config]")
 {
 	Config l_Config;
-	const bool l_Loaded = l_Config.ReadFromFile("data/sandman.conf");
+	bool const l_Loaded = l_Config.ReadFromFile("data/sandman.conf");
 	REQUIRE(l_Loaded == true);
 
-	const std::vector<ControlConfig> l_ControlConfigs = l_Config.GetControlConfigs();
+	std::vector<ControlConfig> const& l_ControlConfigs = l_Config.GetControlConfigs();
 	REQUIRE(l_ControlConfigs.size() == 3);
 	if (l_ControlConfigs.size() > 2)
 	{
@@ -59,19 +59,58 @@ TEST_CASE("Test default config", "[config]")
 			REQUIRE(l_ControlConfigs[2].m_MovingDurationMS == 4000);
 		}
 	}
+
+	REQUIRE(l_Config.GetControlMaxMovingDurationMS() == 100000);
+	REQUIRE(l_Config.GetControlCoolDownDurationMS() == 25);
+
+	std::vector<InputBinding> const& l_InputBindings = l_Config.GetInputBindings();
+	REQUIRE(l_InputBindings.size() == 6);
+	if (l_InputBindings.size() > 5)
+	{
+		{
+			REQUIRE(l_InputBindings[0].m_KeyCode == 310);
+			REQUIRE(std::string(l_InputBindings[0].m_ControlAction.m_ControlName) == "back");
+			REQUIRE(l_InputBindings[0].m_ControlAction.m_Action == Control::ACTION_MOVING_UP);
+		}
+		{
+			REQUIRE(l_InputBindings[1].m_KeyCode == 311);
+			REQUIRE(std::string(l_InputBindings[1].m_ControlAction.m_ControlName) == "back");
+			REQUIRE(l_InputBindings[1].m_ControlAction.m_Action == Control::ACTION_MOVING_DOWN);
+		}
+		{
+			REQUIRE(l_InputBindings[2].m_KeyCode == 308);
+			REQUIRE(std::string(l_InputBindings[2].m_ControlAction.m_ControlName) == "legs");
+			REQUIRE(l_InputBindings[2].m_ControlAction.m_Action == Control::ACTION_MOVING_UP);
+		}
+		{
+			REQUIRE(l_InputBindings[3].m_KeyCode == 305);
+			REQUIRE(std::string(l_InputBindings[3].m_ControlAction.m_ControlName) == "legs");
+			REQUIRE(l_InputBindings[3].m_ControlAction.m_Action == Control::ACTION_MOVING_DOWN);
+		}
+		{
+			REQUIRE(l_InputBindings[4].m_KeyCode == 307);
+			REQUIRE(std::string(l_InputBindings[4].m_ControlAction.m_ControlName) == "elev");
+			REQUIRE(l_InputBindings[4].m_ControlAction.m_Action == Control::ACTION_MOVING_UP);
+		}
+		{
+			REQUIRE(l_InputBindings[5].m_KeyCode == 304);
+			REQUIRE(std::string(l_InputBindings[5].m_ControlAction.m_ControlName) == "elev");
+			REQUIRE(l_InputBindings[5].m_ControlAction.m_Action == Control::ACTION_MOVING_DOWN);
+		}
+	}
 }
 
 TEST_CASE("Test missing schedule", "[schedules]")
 {
 	Schedule l_Schedule;
-	const bool l_Loaded = l_Schedule.ReadFromFile("");
+	bool const l_Loaded = l_Schedule.ReadFromFile("");
 	REQUIRE(l_Loaded == false);
 }
 
 TEST_CASE("Test default (empty) schedule", "[schedules]")
 {
 	Schedule l_Schedule;
-	const bool l_Loaded = l_Schedule.ReadFromFile("data/sandman.sched");
+	bool const l_Loaded = l_Schedule.ReadFromFile("data/sandman.sched");
 	REQUIRE(l_Loaded == true);
 	REQUIRE(l_Schedule.IsEmpty() == true);
 }
@@ -79,19 +118,19 @@ TEST_CASE("Test default (empty) schedule", "[schedules]")
 TEST_CASE("Test invalid schedule", "[schedules]")
 {
 	Schedule l_Schedule;
-	const bool l_Loaded = l_Schedule.ReadFromFile("data/invalidJson.sched");
+	bool const l_Loaded = l_Schedule.ReadFromFile("data/invalidJson.sched");
 	REQUIRE(l_Loaded == false);
 }
 
 TEST_CASE("Test example schedule", "[schedules]")
 {
 	Schedule l_Schedule;
-	const bool l_Loaded = l_Schedule.ReadFromFile("data/example.sched");
+	bool const l_Loaded = l_Schedule.ReadFromFile("data/example.sched");
 	REQUIRE(l_Loaded == true);
 	REQUIRE(l_Schedule.IsEmpty() == false);
 	REQUIRE(l_Schedule.GetNumEvents() == 2);
 
-	const std::vector<ScheduleEvent>& l_Events = l_Schedule.GetEvents();
+	std::vector<ScheduleEvent>& l_Events = l_Schedule.GetEvents();
 	if (l_Events.size() > 1)
 	{
 		REQUIRE(l_Events[0].m_DelaySec == 20);
