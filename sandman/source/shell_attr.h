8a54e412d4bc35f4d41e96a18ac16f12ade58be4
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-08-08 14:49:10 -0500

    move color utility to `shell_attr.h`

--- sandman/source/shell_attr.h
+++ sandman/source/shell_attr.h
@@ -35,7 +35,7 @@ namespace Shell {
 
 		[[nodiscard]] constexpr explicit Attr(Value const attributes) : m_Value{ attributes } {}
 
-		// Default constructor is implicit.
+		// The default constructor is implicit.
 		[[nodiscard]] constexpr Attr() : Attr(Value{A_NORMAL}) {};
 
 		[[nodiscard]] constexpr Attr operator|(Attr const attributes) const
@@ -46,7 +46,6 @@ namespace Shell {
 		struct ColorPair;
 		struct ForegroundColor;
 		struct BackgroundColor;
-		struct Toggler;
 		template <typename...> struct Wrapper;
 
 		template <typename... ParamsT>
@@ -62,12 +61,61 @@ namespace Shell {
 
 	};
 
-	struct Attr::Toggler : Common::Box<bool>
+	struct ForegroundColorIndex : Common::Box<ColorIndex> { using Box::Box; };
+	struct BackgroundColorIndex : Common::Box<ColorIndex> { using Box::Box; };
+
+	// Not making assumptions about what the Curses color macros
+	// are defined as, so this function serves as a well defined mapping
+	// from numeric constants to the Curses color macros.
+	constexpr signed short int GetColorID(ColorIndex const color)
 	{
-		Attr m_Attributes;
-		[[nodiscard]] constexpr explicit Toggler(Attr const attributes, bool const b)
-			: Box{b}, m_Attributes(attributes) {}
+		switch (color)
+		{
+			case ColorIndex::Black  : return COLOR_BLACK  ;
+			case ColorIndex::Red    : return COLOR_RED    ;
+			case ColorIndex::Green  : return COLOR_GREEN  ;
+			case ColorIndex::Yellow : return COLOR_YELLOW ;
+			case ColorIndex::Blue   : return COLOR_BLUE   ;
+			case ColorIndex::Magenta: return COLOR_MAGENTA;
+			case ColorIndex::Cyan   : return COLOR_CYAN   ;
+			case ColorIndex::White  : return COLOR_WHITE  ;
+
+			default: return {};
+		}
+	}
+
+	inline constexpr std::array kColorList{
+		ColorIndex::Black  ,
+		ColorIndex::Red    ,
+		ColorIndex::Green  ,
+		ColorIndex::Yellow ,
+		ColorIndex::Blue   ,
+		ColorIndex::Magenta,
+		ColorIndex::Cyan   ,
+		ColorIndex::White  ,
 	};
+
+	constexpr Attr GetColorPair(ForegroundColorIndex const foregroundColor,
+										 BackgroundColorIndex const backgroundColor)
+	{
+		using Common::Enum::IntCast;
+		using ColorPairID = decltype(GetColorID(std::declval<ColorIndex>()));
+
+		ColorPairID const column{ IntCast(foregroundColor.m_Value) };
+		ColorPairID const row{ IntCast(backgroundColor.m_Value) };
+
+		// Check that it's okay to downcast the `std::size_t` from `size()` to `ColorPairID`.
+		static_assert(kColorList.size() <= 8u and 8u <= std::numeric_limits<ColorPairID>::max());
+
+		ColorPairID const colorPairIndex{
+			// Needs to be static cast to a `ColorPairID`
+			// because operations on `short` integral types
+			// will implicitly promote to non `short` types.
+			static_cast<ColorPairID>(row * ColorPairID{ kColorList.size() } + column)
+		};
+
+		return Attr(Attr::Value{ COLOR_PAIR(colorPairIndex) });
+	}
 
 	template <typename... ObjectsT>
 	struct [[nodiscard]] Attr::Wrapper
@@ -92,12 +140,30 @@ namespace Shell {
 
 	struct Attr::ForegroundColor
 	{
-		Attr m_Attributes;
+		Attr m_Ancillary;
 		ColorIndex m_ColorIndex;
 
 		[[nodiscard]] constexpr ForegroundColor operator|(Attr const attributes) const
 		{
-			return { this->m_Attributes | attributes, m_ColorIndex };
+			return { this->m_Ancillary | attributes, m_ColorIndex };
+		}
+
+		[[nodiscard]] constexpr Attr BuildAttr() const
+		{
+			ForegroundColorIndex const foregroundColor(m_ColorIndex);
+			BackgroundColorIndex const backgroundColor(ColorIndex::Black);
+			Attr const colorPair{ GetColorPair(foregroundColor, backgroundColor) };
+			return m_Ancillary | colorPair;
+		}
+
+		template <typename... ParamsT>
+		[[nodiscard]] constexpr Wrapper<Common::Forward<ParamsT>...>
+			operator()(Common::Forward<ParamsT>... args) const
+		{
+			static_assert(sizeof...(args) > 0u,
+							  "Calling this function without any arguments is probably a mistake.");
+
+			return Wrapper(this->BuildAttr(), std::forward<ParamsT>(args)...);
 		}
 	};
 
@@ -109,12 +175,30 @@ namespace Shell {
 
 	struct Attr::BackgroundColor
 	{
-		Attr m_Attributes;
+		Attr m_Ancillary;
 		ColorIndex m_ColorIndex;
 
 		[[nodiscard]] constexpr BackgroundColor operator|(Attr const attributes) const
 		{
-			return { this->m_Attributes | attributes, m_ColorIndex };
+			return { this->m_Ancillary | attributes, m_ColorIndex };
+		}
+
+		[[nodiscard]] constexpr Attr BuildAttr() const
+		{
+			ForegroundColorIndex const foregroundColor(ColorIndex::White);
+			BackgroundColorIndex const backgroundColor(m_ColorIndex);
+			Attr const colorPair{ GetColorPair(foregroundColor, backgroundColor) };
+			return m_Ancillary | colorPair;
+		}
+
+		template <typename... ParamsT>
+		[[nodiscard]] constexpr Wrapper<Common::Forward<ParamsT>...>
+			operator()(Common::Forward<ParamsT>... args) const
+		{
+			static_assert(sizeof...(args) > 0u,
+							  "Calling this function without any arguments is probably a mistake.");
+
+			return Wrapper(this->BuildAttr(), std::forward<ParamsT>(args)...);
 		}
 	};
 
@@ -126,12 +210,32 @@ namespace Shell {
 
 	struct Attr::ColorPair
 	{
-		Attr m_Attributes;
+		Attr m_Ancillary;
 		std::underlying_type_t<ColorIndex> m_ColorIndexPair;
 
 		[[nodiscard]] constexpr ColorPair operator|(Attr const attributes) const
 		{
-			return { this->m_Attributes | attributes, m_ColorIndexPair };
+			return { this->m_Ancillary | attributes, m_ColorIndexPair };
+		}
+
+		[[nodiscard]] constexpr Attr BuildAttr() const
+		{
+			ForegroundColorIndex const foregroundColor(
+				static_cast<ColorIndex>(m_ColorIndexPair bitand 0b000111));
+			BackgroundColorIndex const backgroundColor(
+				static_cast<ColorIndex>((m_ColorIndexPair bitand 0b111000) >> 3u));
+			Attr const colorPair{ GetColorPair(foregroundColor, backgroundColor) };
+			return m_Ancillary | colorPair;
+		}
+
+		template <typename... ParamsT>
+		[[nodiscard]] constexpr Wrapper<Common::Forward<ParamsT>...>
+			operator()(Common::Forward<ParamsT>... args) const
+		{
+			static_assert(sizeof...(args) > 0u,
+							  "Calling this function without any arguments is probably a mistake.");
+
+			return Wrapper(this->BuildAttr(), std::forward<ParamsT>(args)...);
 		}
 	};
 
@@ -154,7 +258,7 @@ namespace Shell {
 			)
 		};
 
-		return { foregroundColor.m_Attributes | backgroundColor.m_Attributes, colorIndexPair };
+		return { foregroundColor.m_Ancillary | backgroundColor.m_Ancillary, colorIndexPair };
 	}
 
 	[[nodiscard]] constexpr
@@ -163,4 +267,47 @@ namespace Shell {
 	{
 		return foregroundColor | backgroundColor;
 	}
-}
+
+	inline namespace CharacterAttributeConstants
+	{
+
+		using Fg = ForegroundColorIndex;
+		using Bg = BackgroundColorIndex;
+
+		// NOLINTBEGIN(readability-identifier-naming)
+
+		inline constexpr Attr
+			Normal     (A_NORMAL                                                      ),
+			Highlight  (A_STANDOUT                                                    ),
+			Underline  (A_UNDERLINE                                                   ),
+			FlipColor  (A_REVERSE                                                     ),
+			Blink      (A_BLINK                                                       ),
+			Dim        (A_DIM                                                         ),
+			Bold       (A_BOLD                                                        ),
+			Invisible  (A_INVIS                                                       ),
+			Italic     (A_ITALIC                                                      );
+
+		inline constexpr Attr::ForegroundColor
+			Black      {Normal, ColorIndex::Black  },
+			Red        {Normal, ColorIndex::Red    },
+			Green      {Normal, ColorIndex::Green  },
+			Yellow     {Normal, ColorIndex::Yellow },
+			Blue       {Normal, ColorIndex::Blue   },
+			Magenta    {Normal, ColorIndex::Magenta},
+			Cyan       {Normal, ColorIndex::Cyan   },
+			White      {Normal, ColorIndex::White  };
+
+		inline constexpr Attr::BackgroundColor
+			BackBlack  {Normal, ColorIndex::Black  },
+			BackRed    {Normal, ColorIndex::Red    },
+			BackGreen  {Normal, ColorIndex::Green  },
+			BackYellow {Normal, ColorIndex::Yellow },
+			BackBlue   {Normal, ColorIndex::Blue   },
+			BackMagenta{Normal, ColorIndex::Magenta},
+			BackCyan   {Normal, ColorIndex::Cyan   },
+			BackWhite  {Normal, ColorIndex::White  };
+
+		// NOLINTEND(readability-identifier-naming)
+	}
+
+} // namespace Shell
