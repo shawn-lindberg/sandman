bcd4445711d94902bbe1c731b98dd7a5a33ee02e
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-08-09 14:45:40 -0500

    comments

--- sandman/source/logger.h
+++ sandman/source/logger.h
@@ -15,12 +15,34 @@ class Logger
 
 private:
 
+	// String data to be written to output.
+	//
+	// String data is first stored here before being sent to output
+	// in order to transform the data into string form which both
+	// the shell graphics system and the output stream understand.
 	std::ostringstream m_Buffer;
+
+	// Main output destination of the logger to write data to.
 	std::ostream& m_OutputStream;
+
+	// Flag governing whether this logger should write to
+	// shell graphics. This is `false` by default.
+	//
+	// Only the global logger should be able to have this set to true
+	// through a global accessor method. Other loggers should not
+	// be able to set this variable with their member public interface.
 	bool m_ScreenEcho{ false };
 
 public:
 
+	// Construct a logger with a reference to an output stream.
+	//
+	// The output stream is stored internally, and can be written to over the
+	// course of the lifetime of the constructed logger object.
+	//
+	// Since the constructed logger will stored the output stream by reference,
+	// it is important to ensure that the output stream is not 
+	// destroyed, lest the reference to the output stream that the logger stores be invalidated.
 	[[nodiscard]] Logger(std::ostream& outputStream): m_Buffer(), m_OutputStream(outputStream) {}
 
 	[[nodiscard]] constexpr std::ostringstream const& GetBuffer() const
@@ -38,24 +60,71 @@ public:
 		return m_ScreenEcho;
 	}
 
+	// "Lower-level" write function.
+	// This simply writes data to the internal buffer,
+	// then flushes the data to the output destinations.
+	//
+	// The internal buffer should be empty after every call to this function.
 	template <typename T, typename... ParamsT>
 	[[gnu::always_inline]] inline void Write(Common::Forward<T> firstArg,
 														  Common::Forward<ParamsT>... args);
 
 	static constexpr char kFormatEscapeIndicator{ '%' }, kFormatInterpolationIndicator{ '$' };
 
+	static constexpr std::string_view kMissingFormatValue = "`null`";
+
+	// "Lower-level" format write function.
+	// 
+	// Base case for template recursion. 
 	void FormatWrite(std::string_view const formatString);
 
+	// "Lower-level" format write function.
+	// 
+	// Interpolates arguments into the format string
+	// as it writes all characters in the format string to
+	// the buffer.
+	// 
+	// `%$` interpolates the an argument into the format string.
+	// `%%` writes a literal percent sign `%` character.
+	//
+	// If interpolation is denoted with `%$` but there are no more arguments
+	// to interpolation, `::Logger::kMissingFormatValue` is written instead.
+	// 
+	// The arguments are interpolated into the string in the order
+	// that they are passed into this function from left to right.
+	//
+	// If there are more arguments than there are interpolation indicators `%$`
+	// in the format string, the extra arguments are ignored.
 	template <typename T, typename... ParamsT>
 	void FormatWrite(std::string_view formatString, Common::Forward<T> firstArg,
 						  Common::Forward<ParamsT>... args);
 
+	// Object wrapper for formatting.
+	//
+	// Instances of this class can be passed to `Logger::WriteLine`. If so,
+	// the arguments will be interpolated into the
+	// format string and written to the buffer as if
+	// `Logger::FormatWrite` was called with the arguments
+	// that the `Format` instance was constructed with.
 	template <typename... ObjectsT>
 	struct Format
 	{
 		std::tuple<ObjectsT...> m_Objects;
 		std::string_view m_FormatString;
 
+		// Constructor.
+		//
+		// `%$` interpolates the an argument into the format string.
+		// `%%` writes a literal percent sign `%` character.
+		//
+		// If interpolation is denoted with `%$` but there are no more arguments
+		// to interpolation, `::Logger::kMissingFormatValue` is written insteaed.
+		//
+		// The arguments are interpolated into the string in the order
+		// that they are passed into this function from left to right.
+		//
+		// If there are more arguments than there are interpolation indicators `%$`
+		// in the format string, the extra arguments are ignored.
 		template <typename... ParamsT>
 		[[nodiscard]] explicit Format(std::string_view const formatString,
 												Common::Forward<ParamsT>... args)
@@ -64,16 +133,24 @@ public:
 	};
 
 	// Deduction guide.
+	// 
+	// Deduce the type of a `Format` object from the arguments passed to its constructor.
 	template <typename... ParamsT>
 	Format(std::string_view const,
 			 Common::Forward<ParamsT>...) -> Format<Common::Forward<ParamsT>...>;
 
 private:
 
-	static std::mutex ms_Mutex;
-	static std::ofstream ms_File;
-	static Logger ms_GlobalLogger;
+	// Global logger.
+	static Logger ms_Logger;
 
+	// Mutex for global logger.
+	static std::mutex ms_Mutex;
+
+	// The file that the global logger writes to.
+	static std::ofstream ms_File;
+
+	// This is implementation of some traits for compile-time conditionals.
 	struct Traits
 	{
 		template <typename>
@@ -85,40 +162,65 @@ private:
 
 public:
 
+	// Get the global logger.
 	[[nodiscard]] static constexpr Logger const& Get()
 	{
-		return ms_GlobalLogger;
+		return ms_Logger;
 	}
 
+	// Is `true` if `T` is a type that is a conrete instance of the `Format` template class;
+	// is `false` otherwise.
+	//
+	// This can be used for compile-time conditional branching.
 	template <typename T>
 	// NOLINTNEXTLINE(readability-identifier-naming)
 	static constexpr bool IsFormat{ ::Logger::Traits::IsFormat<T>{} };
 
+	// Get reference to mutable screen echo flag of the global logger.
 	[[gnu::always_inline]] [[nodiscard]] inline static bool& GetScreenEchoFlag()
 	{
-		return ms_GlobalLogger.m_ScreenEcho;
+		return ms_Logger.m_ScreenEcho;
 	}
 
+	/// @brief Initializes the global logger such that it can write
+	/// to the file denoted by the passed in file name. If the
+	/// file doesn't exist, then it is automatically created.
+	///
+	/// @warning This does not initialize the shell graphics system.
+	///
+	/// @returns `true` on success, `false` otherwise.
 	[[nodiscard]] static bool Initialize(char const* const logFileName);
 
+	/// Close the file associated with the global logger.
 	static void Uninitialize();
 
+	// "Higher-level" write function.
+	//
+	// Using the global logger's `Write` method,
+	// writes a string composed of a timestamp followed by
+	// the arguments of this function followed by a newline character `'\n'`.
 	template <typename... ParamsT>
 	[[gnu::always_inline]] inline static void WriteLine(Common::Forward<ParamsT>... args);
 
-	static constexpr Shell::Attr::Value kDefaultColorPair
-	{
-		Shell::GetColorPair(Shell::ForegroundColorIndex(Shell::ColorIndex::White),
-								  Shell::BackgroundColorIndex(Shell::ColorIndex::Black)).m_Value
-	};
-
+	// "std::vprintf" style write function.
+	//
+	// The function takes arguments that are interpreted exactly how
+	// `std::vprintf` would interpret them, and writes the data using the global logger.
+	//
+	// Internally calls `std::vsnprintf` to format the data,
+	// then calls `::Logger::WriteLine` to write the data.
 	template <auto = nullptr, std::size_t kStringBufferCapacity = 1u << 11u>
-	[[gnu::format(printf, 1, 0)]] static bool FormatWriteLine(char const* format,
+	[[gnu::format(printf, 1, 0)]] static bool FormatWriteLine(char const* const format,
 																				 std::va_list argumentList);
 
+	// "std::printf" style write function.
+	// 
+	// This function takes arguments that are interpreted exactly how
+	// `std::printf` would interpret them, and writes the data using the global logger.
+	// 
+	// Internally calls `::Logger::FormatWriteLine(char const* const, std::va_list)`.
 	template <auto = nullptr, std::size_t kStringBufferCapacity = 1u << 11u>
-	[[gnu::format(printf, 1, 2)]] static bool FormatWriteLine(char const* format, ...);
-
+	[[gnu::format(printf, 1, 2)]] static bool FormatWriteLine(char const* const format, ...);
 
 };
 
