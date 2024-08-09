db7d72d2c32d6bc0da3511913dfc02a9b4862459
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-08-08 23:17:34 -0500

    comments

--- sandman/source/common/string.h
+++ sandman/source/common/string.h
@@ -65,22 +65,38 @@ class Common::String
 			m_OnDecrementStringLength            (onDecrementStringLengthListener)
 		{}
 
+		// Insert a character at any valid index in the string.
+		// Can insert a character at the end by inserting at
+		// index string length; that is what `String::Push` does.
+		//
+		// Inserting a character pushes all characters after it one position to the right.
 		constexpr bool Insert(typename Data::size_type const index, CharT const character)
 		{
+			// Can insert at any index in the string or at the end if index is string length.
+			// Can only insert a character if the string is not at maximum capacity.
 			if (index <= m_StringLength and m_StringLength < kMaxStringLength)
 			{
+				// Starting from the index of the null terminator which is at
+				// index string length, will iterate leftward shifting each character
+				// to the right by one position until reached index to insert
+				// the the new character at.
 				for (typename Data::size_type i{ m_StringLength }; i > index; --i)
 				{
 					m_OnStringUpdate(i, m_Data[i] = m_Data[i - 1u]);
 				}
 
+				// Use assignment to insert the character at a position.
+				// Also call the event listener.
 				m_OnStringUpdate(index, m_Data[index] = character);
 
+				// Increment the string length and null terminate the string.
 				m_Data[++m_StringLength] = '\0';
 
+				// Success.
 				return true;
 			}
 
+			// Failure.
 			return false;
 		}
 
@@ -89,17 +105,20 @@ class Common::String
 		// Returns `true` on success and `false` otherwise.
 		constexpr bool Push(CharT const character)
 		{
+			// Can only push a character if the string is not at max capacity.
 			if (m_StringLength < kMaxStringLength)
 			{
-				// insert the character at index string length and call the event listener.
+				// Insert the character at index string length and call the event listener.
 				m_OnStringUpdate(m_StringLength, m_Data[m_StringLength] = character);
 
 				// Update the string length and null terminate the string.
 				m_Data[++m_StringLength] = '\0';
 
+				// Success.
 				return true;
 			}
 
+			// Failure.
 			return false;
 		}
 
@@ -117,6 +136,10 @@ class Common::String
 				// will iterate rightward shifting each character to the left by one position,
 				// until but not including he null character string terminator at the index of
 				// string length.
+				// 
+				// Since started from index of character to remove,
+				// the character to remove is overwritten with the character directly
+				// to its right, effectively removing it from the string.
 				// 
 				// It's impossible for the string length to be zero here,
 				// because the removal index has to be less than the string length,
@@ -137,9 +160,11 @@ class Common::String
 				// Call the event listener.
 				m_OnDecrementStringLength(m_StringLength);
 
+				// Success.
 				return true;
 			}
 
+			// Failure.
 			return false;
 		}
 
@@ -155,6 +180,7 @@ class Common::String
 			// Call the event listener.
 			m_OnClear();
 
+			// Success.
 			return static_cast<void>(true);
 		}
 
