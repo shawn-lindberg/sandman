8c79a97199575348f9115adf57323ae7b6d3ed34
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-08-10 13:57:05 -0500

    use `std::va_list` function

--- sandman/source/input.cpp
+++ sandman/source/input.cpp
@@ -298,7 +298,7 @@ void Input::CloseDevice(bool wasFailure, char const* format, ...)
 	va_list arguments;
 	va_start(arguments, format);
 
-	Logger::WriteAttrFormattedLine(Shell::Red, format, arguments);
+	Logger::WriteFormattedVarArgsListLine(Shell::Red, format, arguments);
 	
 	va_end(arguments);
 		
