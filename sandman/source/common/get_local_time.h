ecc09ad9d0949441c17c1fb0e7590602d00fc5ea
Adrian <73155005+adrian-javid@users.noreply.github.com>
2024-08-10 17:14:22 -0500

    explicit type

--- sandman/source/common/get_local_time.h
+++ sandman/source/common/get_local_time.h
@@ -13,7 +13,7 @@ namespace Common
 	{
 		auto const timePoint(std::chrono::system_clock::now());
 
-		auto const arithmeticTimeValue{ std::chrono::system_clock::to_time_t(timePoint) };
+		std::time_t const arithmeticTimeValue{ std::chrono::system_clock::to_time_t(timePoint) };
 		static_assert(std::is_arithmetic_v<decltype(arithmeticTimeValue)>);
 
 		return std::localtime(&arithmeticTimeValue);
